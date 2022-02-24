7929401b5344af3259afae8f3a7add83edd587ad
Hi, my name is Tahubulat <99246083+ahubulat@users.noreply.github.com>
2022-02-24 19:51:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+{
+    "ownerNumber": "6285643774316",
+    "ownerName": "Tahu Botz",
+    "limitCount": 30,
+    "gamewaktu": 60,
+    "botName": "Tahu Botz",
+    "pathImg": "./media/Itsuki.jpg",
+    "apikey": "apikey is here",
+    "fake": "ALAN BOTZ VERIFIED",
+    "sesidInsta": "default",
+    "sessionName": "aqulzz",
+    "txtDonasi": "GOPAY : 085718366706\nSaweeria :https://saweria.co/alanbotz\nPulsa : 085718366706",
+    "gcount": {
+        "prem": 40,
+        "user": 20
+    },
+    "emote":{
+        "free": "◽",
+        "owner": "◾",
+        "limit": "🔸",
+        "glimit": "🔹",
+        "prem": "💰"
+    }
+}
